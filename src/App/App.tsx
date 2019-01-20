add30d87ed9482c92bb1400baa3bf96b7a14e6f8
Uffe Hellum (Zen3) <v-ufhel@microsoft.com>
2019-01-19 18:33:25 -0800

    redux

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -7,7 +7,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
           </p>
@@ -20,6 +19,9 @@ class App extends Component {
             Learn React
           </a>
         </header>
+
+        <img src={logo} className="App-logo" alt="logo" />
+
       </div>
     )
   }
